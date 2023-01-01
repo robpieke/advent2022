6c737750f7504417102a2519303d472a93c5aa2b
robpieke <robpieke@gmail.com>
2023-01-01 19:04:22 +0000

    Making the linter happy

--- 18_R/18a.R
+++ 18_R/18a.R
@@ -3,11 +3,16 @@ cubes <- as.matrix(read.csv("input.txt", header = FALSE))
 exposed <- nrow(cubes) * 6
 
 for (x in 1:3) {
-  cubes <- cubes[order(cubes[,x], cubes[,x%%3+1], cubes[,(x+1)%%3+1]),]
+  cubes <- cubes[order(cubes[, (x - 1) %% 3 + 1],
+                       cubes[, (x + 0) %% 3 + 1],
+                       cubes[, (x + 1) %% 3 + 1]), ]
   dists <- abs(diff(cubes))
   neighbour <- c(0, 0, 0)
-  neighbour[(x+1)%%3+1] <- 1
-  exposed <- exposed - 2 * sum(apply(dists, 1, function(x) identical(as.numeric(x), neighbour)))
+  neighbour[(x + 1) %% 3 + 1] <- 1
+  is_neighbour <- function(x) {
+    return(identical(as.numeric(x), neighbour))
+  }
+  exposed <- exposed - 2 * sum(apply(dists, 1, is_neighbour))
 }
 
 print(exposed)
