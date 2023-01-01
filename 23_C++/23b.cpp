0a585df43fbf6e89eae7dd083875e6dc9fc29bd0
robpieke <robpieke@gmail.com>
2023-01-01 21:25:26 +0000

    Day 23

---
+++ 23_C++/23b.cpp
@@ -0,0 +1,79 @@
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <set>
+
+int main()
+{
+    std::ifstream f("input.txt");
+    std::string line;
+    int y = 0;
+    using Coord = std::pair<int, int>;
+    std::set<Coord> elves;
+    while (std::getline(f, line))
+    {
+        for (int x = 0; x < line.length(); ++x)
+            if (line[x] == '#')
+                elves.insert({x, y});
+        ++y;
+    }
+
+    int step = 0;
+    while (true)
+    {
+        std::map<Coord, Coord> proposed;
+        std::array<int, 4> masks = { 0b00000111, 0b11100000, 0b00101001, 0b10010100 };
+        std::array<int, 4> dx = { 0, 0, -1, 1}; 
+        std::array<int, 4> dy = { -1, 1, 0, 0};
+
+        bool moveMade = false;
+
+        for (const auto &elf : elves)
+        {
+            Coord next = elf;
+            int neighbours = 
+                (elves.count({elf.first - 1, elf.second - 1}) << 0) +
+                (elves.count({elf.first    , elf.second - 1}) << 1) +
+                (elves.count({elf.first + 1, elf.second - 1}) << 2) +
+                (elves.count({elf.first - 1, elf.second    }) << 3) +
+                (elves.count({elf.first + 1, elf.second    }) << 4) +
+                (elves.count({elf.first - 1, elf.second + 1}) << 5) +
+                (elves.count({elf.first    , elf.second + 1}) << 6) +
+                (elves.count({elf.first + 1, elf.second + 1}) << 7);
+            if (neighbours)
+            {
+                for (int mask = 0; mask < masks.size(); ++mask)
+                {
+                    int idx = (mask + step) % masks.size();
+                    if ((neighbours & masks[idx]) == 0)
+                    {
+                        next = {elf.first + dx[idx], elf.second + dy[idx]};
+                        break;
+                    }
+                }
+                if (proposed.count(next))
+                {
+                    proposed[proposed[next]] = proposed[next];
+                    proposed[next] = {INT_MAX, INT_MAX};
+                    next = elf;
+                }
+                else
+                    moveMade = true;
+            }
+            proposed[next] = elf;
+        }
+        elves.clear();
+        for (const auto &elf : proposed)
+            if (elf.second != std::make_pair(INT_MAX, INT_MAX))
+                elves.insert(elf.first);
+
+        ++step;
+        if (!moveMade)
+            break;
+    }
+
+    std::cout << step << std::endl;
+
+    return 0;
+}
